eff4149af6dbea16250cd1ef9513ea875e4e1026
Landry Ilunga <landryilunga@MacBook-Pro.local>
2023-06-14 01:18:45 +0200

    created registration page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./component/login/login.component";
+import {Constants} from "./common/Constants";
+import {RegistrationComponent} from "./component/registration/registration.component";
 
 const routes: Routes = [
-  {path: '', component: LoginComponent}
+  {path: '', component: LoginComponent},
+  {path: Constants.REGISTRATION_ROUTE, component: RegistrationComponent}
 ];
 
 @NgModule({
---
+++ src/app/common/Constants.ts
@@ -0,0 +1,4 @@
+export class Constants{
+  static LOGIN_ROUTE = 'login';
+  static REGISTRATION_ROUTE = 'registration'
+}
