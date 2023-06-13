fa147fc1c616e05336e24325520e927a8e71c0ff
Landry Ilunga <landryilunga@MacBook-Pro.local>
2023-06-13 23:16:38 +0200

    creted login page

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  loginForm = new FormGroup({
+    username: new FormControl('',[Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    rememberMe: new FormControl()
+  })
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit(){
+    if (this.loginForm.valid){
+      alert('navigate to otp');
+    }
+  }
 
 }
