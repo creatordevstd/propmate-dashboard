22caa8b5522a379bb9d92aae542cdead1cca1223
Landry Ilunga <landryilunga@MacBook-Pro.local>
2023-06-14 16:55:25 +0200

    created header and footer and navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,13 +15,19 @@ import { LoginComponent } from './component/login/login.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { RegistrationComponent } from './component/registration/registration.component';
 import { OtpComponent } from './component/otp/otp.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { NavigationComponent } from './component/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegistrationComponent,
-    OtpComponent
+    OtpComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
--- src/app/component/footer/footer.component.ts
+++ src/app/component/footer/footer.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+import * as feather from "feather-icons";
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements AfterViewInit{
 
+  ngAfterViewInit() {
+    feather.replace();
+  }
 }
