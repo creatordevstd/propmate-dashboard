e8e4ec072d51e87bb0baa4c49f3eeb0bd1bcbd5c
creatordevstd <133219334+creatordevstd@users.noreply.github.com>
2023-06-20 17:06:27 +0200

    Create dashboard (#6)
    * added dashboard page
    
    * dashboard page
    
    ---------
    
    Co-authored-by: Landry Ilunga <landryilunga@MacBook-Pro.local>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import {Constants} from "./common/Constants";
 import {RegistrationComponent} from "./component/registration/registration.component";
 import {OtpComponent} from "./component/otp/otp.component";
 import {NavigationComponent} from "./component/navigation/navigation.component";
+import {DashboardComponent} from "./component/dashboard/dashboard.component";
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: Constants.REGISTRATION_ROUTE, component: RegistrationComponent},
   {path: Constants.OTP_ROUTE, component: OtpComponent},
   {path: Constants.DASHBOARD_ROUTE, component: NavigationComponent},
+  {path: Constants.DASHBOARD_OVERVIEW_ROUTE, component: DashboardComponent},
 ];
 
 @NgModule({
